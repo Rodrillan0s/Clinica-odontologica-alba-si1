9032e9c26121a4221e3026622f89454ae4040e8a
Eddy-ST <eddymarcelorey@gmail.com>
2026-04-06 15:04:12 -0400

    auth_routes.py modificado para la sincronizacion a la BD

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,6 @@
 from app import create_app
-
+from app.routes.test_routes import test_routes 
 app=create_app()
-
+app.register_blueprint(test_routes)
 if __name__=='__main__':
     app.run(debug=True)
