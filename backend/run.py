88d3d22d9858794a336071784210077a81b8da76
Rodrillan0s <llanosvinayarodrigo@gmail.com>
2026-04-16 01:42:34 -0400

    añadiendo api de prueba

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,11 @@
 from app import create_app
+
+from app.routes.pacientes_routes import paciente_routes 
+
+
 app=create_app()
+
+app.register_blueprint(paciente_routes)
 
 if __name__=='__main__':
     app.run(debug=True)
